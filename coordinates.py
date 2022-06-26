a2ea02816c995518680d781684224b6692a11c7b
RGBsrs <grjanko.d.a@gmail.com>
2022-06-26 19:43:22 +0300

    add get coordinates method

--- coordinates.py
+++ coordinates.py
@@ -1,4 +1,7 @@
+from subprocess import Popen, PIPE
 from typing import NamedTuple
+
+#from exeptions import GantGetCoordinates
 
 
 class Coordinates(NamedTuple):
@@ -9,4 +12,20 @@ def get_coordinates() -> Coordinates:
     """
     Get the coordinates from the user.
     """
-    return Coordinates(longitude=0.0, latitude=0.0)
+
+    process = Popen(["whereami"], stdout=PIPE)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+    if err is not None or exit_code != 0:
+        print("Error:", err)
+    output_lines = output.decode().strip().lower().split("\n")
+    latitude = longitude = None
+    for line in output_lines:
+        if line.startswith("latitude"):
+            latitude = float(line.split(":")[1])
+        if line.startswith("longitude"):
+            longitude = float(line.split(":")[1])
+    return Coordinates(longitude=longitude, latitude=latitude)
+
+if __name__ == "__main__":
+    print(get_coordinates())
