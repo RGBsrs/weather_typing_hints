7667edbb3ca25f544ec525fcd8786c58e1ae1bf8
RGBsrs <grjanko.d.a@gmail.com>
2022-06-28 16:37:20 +0300

    add txt files to gitignore

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 from coordinates import get_coordinates
 from weather_api_service import get_weather
 from weather_formatter import format_weather
+from history import save_weather
 
 def main():
     coordinates = get_coordinates()
